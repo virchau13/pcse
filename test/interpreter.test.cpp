6e0e00be00469bea2e9e43755eac657fccb25e90
virchau13 <computernerd7777@gmail.com>
2020-07-24 19:50:19 +0800

    added invalid testing

--- test/interpreter.test.cpp
+++ test/interpreter.test.cpp
@@ -54,4 +54,36 @@ TEST_CASE("INTERPRETING", "[interpreter]"){
 			REQUIRE(env.out.str() == correct);
 		}
 	}
+	for(const auto& file : fs::directory_iterator("test/invalid-files")){
+		const std::string name = file.path().filename().string();
+		INFO("File is " << name);
+		if(!endsWith(name, ".in.pcse")) continue; /* we don't want to look at this file */
+
+		std::string contents = readFile(file.path().c_str());
+		/* Lexer::Lexer uses a std::string_view, so we have to destroy it _before_ contents */
+		{
+			std::string outname = file.path().c_str();
+			// ".in.pcse" => ".out"
+			{
+				std::string out = ".out";
+				int len = strlen(".in.pcse") - out.size();
+				while(len--){
+					outname.pop_back();
+				}
+				for(size_t i = 0; i < out.size(); i++){
+					outname[i + outname.size() - out.size()] = out[i];
+				}
+			}
+			const std::string correct = readFile(outname);
+			std::string errmsg = "";
+#define CATCH(err) catch(err& e){ errmsg += #err; errmsg += ": "; errmsg += e.what(); }
+			try {
+				Lexer lex(contents);
+				Parser parser(lex.output);
+				Env env(lex.identifier_count);
+				parser.run(env);
+			} CATCH(LexError) CATCH(ParseError) CATCH(TypeError) CATCH(RuntimeError);
+			REQUIRE(errmsg == correct);
+		}
+	}
 }
